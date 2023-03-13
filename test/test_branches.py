1148687a33be6b8fcf169905a3cc33f8274a5db5
Quinn Thibeault <qthibeau@asu.edu>
2023-03-13 11:52:14 -0700

    Create test for branch tree conversion into kripke structure set

--- test/test_branches.py
+++ test/test_branches.py
@@ -32,3 +32,50 @@ def test_branches():
     assert false_child.condition == Condition("x2", Comparison.LTE, 20, False)
     assert len(false_child.true_children) == 0
     assert len(false_child.false_children) == 0
+
+
+def func2(x, y, z):
+    if x <= 1:
+        if y <= 2:
+            ...
+        else:
+            ...
+
+        if z <= 3:
+            ...
+        else:
+            ...
+    else:
+        if y >= 4:
+            ...
+        else:
+            ...
+
+    if z >= 5:
+        ...
+    else:
+        ...
+
+
+def _first_nonleaf(trees: list[BranchTree]) -> BranchTree:
+    if len(trees) == 0:
+        raise ValueError("No trees provided")
+
+    for tree in trees:
+        if len(tree.true_children) > 0 or len(tree.false_children) > 0:
+            return tree
+
+    raise ValueError("All trees in collection are leaves")
+
+
+def test_kripke_conversion():
+    trees = BranchTree.from_function(func2)
+    assert len(trees) == 2
+
+    tree = _first_nonleaf(trees)
+    assert len(tree.true_children) == 2
+    assert len(tree.false_children) == 1
+
+    kripkes = tree.as_kripke()
+    assert len(kripkes) == 2
+    assert all(len(k.states) == 4 for k in kripkes)
