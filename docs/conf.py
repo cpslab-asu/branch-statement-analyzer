dff1c2d57df0285fd8390a02aa82c225c17836dd
Quinn Thibeault <qthibeau@asu.edu>
2023-03-13 17:39:31 -0700

    Add doctest extension to sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.doctest", "sphinx_rtd_theme"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
